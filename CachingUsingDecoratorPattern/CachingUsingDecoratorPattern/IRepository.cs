be4eaf5378fe5dddda6e709b40ec52313f9cbf16
Smriti Srivastava <ssrivastava@tavisca.com>
2018-09-11 14:34:22 +0530

    Added ProductCachingService Console App

---
+++ CachingUsingDecoratorPattern/CachingUsingDecoratorPattern/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CachingUsingDecoratorPattern
+{
+    interface IRepository
+    {
+        List<Product> GetAllProducts();
+        Product GetProductById(int id);
+    }
+}
